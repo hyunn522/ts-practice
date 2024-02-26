f0d69ed9d9d75b946a3d63b027c144f3f82fbec4
hyunn522 <tjgus52213@gmail.com>
2024-02-26 19:03:43 +0900

    Fix: 보안 취약점 알림 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Reset } from 'styled-reset';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { RecoilRoot } from 'recoil';
 import Router from './pages';
 
 const App = () => {
   return (
-    <>
+    <RecoilRoot>
       <Reset />
       <Router />
-    </>
+    </RecoilRoot>
   );
 };
 
--- src/store/cartStore.tsx
+++ src/store/cartStore.tsx
@@ -1,26 +1,29 @@
-import { create, createStore } from 'zustand';
+// import { create, createStore } from 'zustand';
 
-interface Cart {
-  cartStore: string;
-  cartMenus: [];
-  cartSum: number;
-}
+// interface Cart {
+//   cartStore: string;
+//   cartMenus: [];
+//   cartSum: number;
+// }
 
-const initialCart: Cart = { cartStore: '없음', cartMenus: [], cartSum: 0 };
+// const initialCart: Cart = { cartStore: '없음', cartMenus: [], cartSum: 0 };
 
-const useCartStore = create((set, get) => ({
-  cartStore: initialCart.cartStore,
-  cartMenus: initialCart.cartMenus,
-  cartSum: initialCart.cartSum,
+// const useCartStore = create((set, get) => ({
+//   cartStore: initialCart.cartStore,
+//   cartMenus: initialCart.cartMenus,
+//   cartSum: initialCart.cartSum,
 
-  // cart 초기화하는 함수
-  setInit: () => {
-    set(() => ({
-      cartStore: initialCart.cartStore,
-      cartMenus: initialCart.cartMenus,
-      cartSum: initialCart.cartSum,
-    }));
-  },
+//   // cart 초기화하는 함수
+//   setInit: () => {
+//     set(() => ({
+//       cartStore: initialCart.cartStore,
+//       cartMenus: initialCart.cartMenus,
+//       cartSum: initialCart.cartSum,
+//     }));
+//   },
 
-  // cart에 menu 추가하는 함수
-}));
+//   // cart에 menu 추가하는 함수
+// }));
+
+const cartStore = () => {};
+export default cartStore;
